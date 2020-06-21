090032c385c38ae9c045a669e6fb0dc7d4f5f965
techbeast-org <arum0010@e.ntu.edu.sg>
2020-06-21 22:51:08 +0800

    minor touch up

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ var client = mqtt.connect("ws://<AWS-EC2-IP-ADDRESS>:9001", {
 sensor.read(11, 4, function (err, temperature, humidity) {
   //read sensor data from raspberry pi pin GPIO 4
   function timer(temp, humid) {
-    sensor.read(11, 4, function (err, temperature, humidity) {
       client.publish(
         "mqtt/dht",
         JSON.stringify(`Temperature:${temp}C & Humidity:${humid}%`) //convert number to string
       ); //publish sensor data to broker on topic mqtt/dht
 
       console.log("topic published to the broker");
-    });
+   
   }
 
   setInterval(timer, 5000, temperature, humidity); // send sensor data to broker every 5 seconds
